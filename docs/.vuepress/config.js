ae440d0e80119a3df058bea3b3c61cf8f55b3a8c
Xelzs <contact@axelsimonet.fr>
2021-11-30 15:03:32 +0100

    feat: init repo

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Hello, VuePress!',
+  description: 'This is my first VuePress site',
+  bundler: '@vuepress/bundler-vite',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+};
